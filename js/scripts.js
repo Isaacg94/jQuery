d3fe65d6695880e479f06c06f89298195357ae4f
Isaacg94 <7248zack@gmail.com>
2019-08-21 09:30:30 +0300

    add initial page and all files and folders

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('h1').click(function(){
+    alert("This is a heading tag")
+    alert("I told you this was an HEADER!")
+  });
+
+  $('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  $('img').click(function(){
+    alert("This is an image")
+  });
+});
